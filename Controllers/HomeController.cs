0fedbeb856b7481391a21daa9cfd65598bd09d52
jvictor2k <jonvicej@gmail.com>
2022-08-21 22:36:31 -0300

    Pasta Components e CarrinhoCompraResumo criado, Home alterada para exibir carrinho ao colocar produtos.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SkinsSite.Models;
+using SkinsSite.Repositories.Interfaces;
+using SkinsSite.ViewModels;
 using System.Diagnostics;
 
 namespace SkinsSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISkinRepository _skinRepository;
+
+        public HomeController(ISkinRepository skinRepository)
+        {
+            _skinRepository = skinRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                SkinsPreferidas = _skinRepository.SkinsPreferidas
+            };
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
