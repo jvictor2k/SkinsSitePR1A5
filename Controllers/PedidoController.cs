b6cad8ffb13c8bbe9c8f3465d64c551135b2f2dc
jvictor2k <jonvicej@gmail.com>
2022-09-19 21:44:41 -0300

    Criação de PedidoController e view Checkout.

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkinsSite.Models;
+using SkinsSite.Repositories.Interfaces;
+
+namespace SkinsSite.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
