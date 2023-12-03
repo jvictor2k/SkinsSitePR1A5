4011bd3485218624fc0ca27046141783c2d9bb4e
jvictor2k <jv.pereira2k@gmail.com>
2023-12-03 09:45:10 -0300

    Confirmação de envio para itens na área Admin

--- Areas/Admin/Controllers/AdminInventarioItens.cs
+++ Areas/Admin/Controllers/AdminInventarioItens.cs
@@ -65,9 +65,75 @@ namespace SkinsSite.Areas.Admin.Controllers
                 viewModelList.Add(viewModel);
             }
 
-            viewModelList = viewModelList.OrderByDescending(item => item.InventarioItem.Solicitado).ToList();
+            viewModelList = viewModelList.OrderByDescending(item => item.InventarioItem.Solicitado)
+                                         .ThenBy(item => item.InventarioItem.ItemEntregueEm.HasValue)
+                                         .ToList();
 
             return View(viewModelList);
         }
+
+        [HttpPost]
+        public IActionResult ConfirmarEnvios(int[] itensSelecionados)
+        {
+            if (itensSelecionados != null && itensSelecionados.Length > 0)
+            {
+                foreach (var itemId in itensSelecionados)
+                {
+                    var inventarioItem = _AppDbContext.InventarioItens.FirstOrDefault(item => item.InventarioItemId == itemId);
+
+                    if (inventarioItem != null)
+                    {
+                        inventarioItem.ItemEntregueEm = DateTime.Now;
+
+                        _AppDbContext.SaveChanges();
+
+                        // Verificar se todos os outros itens relacionados ao mesmo PedidoDetalhe foram entregues
+                        var todosItensEntregues = _AppDbContext.InventarioItens
+                            .Where(item => item.PedidoDetalheId == inventarioItem.PedidoDetalheId)
+                            .All(item => item.ItemEntregueEm != null);
+
+                        if (todosItensEntregues)
+                        {
+                            // Se todos os itens foram entregues, atualizar o PedidoDetalheEntregueEm
+                            var pedidoDetalhe = _AppDbContext.PedidoDetalhes
+                                .FirstOrDefault(pd => pd.PedidoDetalheId == inventarioItem.PedidoDetalheId);
+
+                            if (pedidoDetalhe != null)
+                            {
+                                pedidoDetalhe.PedidoDetalheEntregueEm = DateTime.Now;
+
+                                _AppDbContext.SaveChanges();
+
+                                // Verificar se todos os outros PedidoDetalhe relacionados ao mesmo PedidoId foram entregues
+                                var todosDetalhesEntregues = _AppDbContext.PedidoDetalhes
+                                    .Where(pd => pd.PedidoId == pedidoDetalhe.PedidoId)
+                                    .All(pd => pd.PedidoDetalheEntregueEm != null);
+
+                                if (todosDetalhesEntregues)
+                                {
+                                    // Se todos os detalhes foram entregues, atualizar o PedidoEntregueEm
+                                    var pedido = _AppDbContext.Pedidos
+                                        .FirstOrDefault(p => p.PedidoId == pedidoDetalhe.PedidoId);
+
+                                    if (pedido != null)
+                                    {
+                                        pedido.PedidoEntregueEm = DateTime.Now;
+
+                                        _AppDbContext.SaveChanges();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                _AppDbContext.SaveChanges();
+
+                TempData["MensagemSucesso"] = "Confirmações de Entrega Feitas com Sucesso!";
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ Migrations/20231203090610_AdicionandoEntregasIndividuais.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkinsSite.Migrations
+{
+    public partial class AdicionandoEntregasIndividuais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PedidoDetalheEntregueEm",
+                table: "PedidoDetalhes",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ItemEntregueEm",
+                table: "InventarioItens",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PedidoDetalheEntregueEm",
+                table: "PedidoDetalhes");
+
+            migrationBuilder.DropColumn(
+                name: "ItemEntregueEm",
+                table: "InventarioItens");
+        }
+    }
+}
